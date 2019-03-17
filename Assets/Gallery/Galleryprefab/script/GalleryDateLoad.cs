9846410aa46805e1300be66a2cae75e18a386841
maekawatakuto <ga000000385507@outlook.jp>
2019-03-17 12:57:12 +0900

    no message

---
+++ Assets/Gallery/Galleryprefab/script/GalleryDateLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalleryDateLoad : MonoBehaviour {
+    SaveData Load;
+	// Use this for initialization
+	void Start () {
+        SaveData Load =SaveData.Instance;
+        Load.Reload();
+    }
+	
+	// Update is called once per frame
+	void Loader () {
+        
+	}
+
+    void sapmle()
+    {
+        //var chapter = Chapter
+        //if()
+    }
+}

63f43f95f343e3e11aa791dcc7031ef1f6f5e7b2
Zukky_13 <www.kase@gmail.com>
2019-03-17 10:32:01 +0900

    no message

---
+++ Assets/Others/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    [SerializeField] ChapterData chapterData;
+    Chapter currentChapter;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += ((scene, mode) => { }        );
+    }
+}

6185c182a9c7079f778c2ab31cda57517e874a76
maekawatakuto <ga000000385507@outlook.jp>
2019-03-17 12:08:26 +0900

    拡大縮小実装

---
+++ Assets/Gallery/Galleryprefab/script/ChangeImage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeImage : MonoBehaviour {
+    Image image;
+    Color color;
+	// Use this for initialization
+	void Start () {
+        image=GetComponent<Image>();
+        color = new Color(1,1,1,1);
+	}
+
+    // Update is called once per frame
+    public void changeColor()
+    {
+        image.color = color;
+    }
+}
---
+++ Assets/Gallery/Galleryprefab/script/Scale.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Scale : MonoBehaviour
+{
+    
+    [SerializeField]
+    GameObject image1;
+    [SerializeField]
+    GameObject image2;
+    private bool flag = false;
+   public void Scaler()
+    {
+        flag = !flag;
+        if (flag)
+        {
+            iTween.ScaleAdd(gameObject, iTween.Hash("y", 1f, "time", 1f));
+            image1.SetActive(false);
+            image2.SetActive(false);
+        }
+        else if (!flag) {
+            iTween.ScaleAdd(gameObject, iTween.Hash("y", -1f, "time", 1f));
+            image1.SetActive(true);
+            image2.SetActive(true);
+        }
+    }
+}
